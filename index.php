d264a9241c57c2ae6e31e9bdeec64c151d56f74a
Harsh882002 <harshpatle491@gmail.com>
2024-12-12 15:27:05 +0530

    login page

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
 
-echo "Hello";
+
 ?>
+<body>
+    <label for="">Enter UserName</label>
+    <input type="text" placeholder="Enter Username" id="username">
+    <br><br>
+    <label for="">Password</label>
+    <input type="text" placeholder="Enter password" id="password">
+    <br><br>
+    <button id="submit">Login</button>
+</body>
